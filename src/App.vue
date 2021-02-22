e2f068a3ac089944fdc017ed01aad111215dcb74
Yucheng <1035487902@qq.com>
2021-02-22 20:46:23 +0800

    draft

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,111 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+
+      <v-main :class="[setPosition]">
+        <v-form>
+            <v-container>
+              <v-row class="justify-center">
+                <v-col cols="8">
+                  <v-text-field
+                    v-model="message"
+                    prepend-inner-icon="mdi-search-web"
+                    :prepend-icon="icon"
+                    filled
+                    clear-icon="mdi-close-circle"
+                    clearable
+                    label="Search"
+                    type="text"
+                    @click:prepend="changeIcon"
+                    @click:clear="clearMessage"
+                  >
+                      
+                    <template v-slot:append-outer>
+              
+                      <v-btn
+                        style="top: -14px"
+                        fab
+                        dark
+                        @click="jumpSearch"
+                      >
+                        <v-icon>
+                          mdi-text-box-search
+                        </v-icon>
+                      </v-btn>
+                      </template>
+                  </v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+        </v-form>
+            <router-view/>
+      </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data: () => ({
+      password: 'Password',
+      show: false,
+      message: 'Type to search',
+      iconIndex: 0,
+      setPosition: 'align-center',
+      icons: [
+        'mdi-emoticon',
+        'mdi-emoticon-cool',
+        'mdi-emoticon-dead',
+        'mdi-emoticon-excited',
+        'mdi-emoticon-happy',
+        'mdi-emoticon-neutral',
+        'mdi-emoticon-sad',
+        'mdi-emoticon-tongue',
+      ],
+    }),
+
+    computed: {
+      icon () {
+        return this.icons[this.iconIndex]
+      },
+    },
+
+    methods: {
+      jumpSearch () {
+        this.setPosition = 'align-auto'
+        this.$router.push('/about')
+      },
+      sendMessage () {
+        this.resetIcon()
+        this.clearMessage()
+      },
+      clearMessage () {
+        this.message = ''
+      },
+      resetIcon () {
+        this.iconIndex = 0
+      },
+      changeIcon () {
+        this.iconIndex === this.icons.length - 1
+          ? this.iconIndex = 0
+          : this.iconIndex++
+      },
+    },
   components: {
-    HelloWorld
-  }
-}
+  },
+
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-container>
+     <v-row
+     v-for="n in 4"
+     :key = 'n'
+     >
+       <v-col>
+        <v-skeleton-loader
+          class="mx-auto"
+          max-width="1000"
+          type="card"
+        ></v-skeleton-loader>
+      </v-col>
+     </v-row>
+    </v-container>
+  </div> 
+</template>
+
+<script>
+  export default {
+    // Vuetify components provide
+    // a theme variable that is
+    // used to determine dark
+    inject: {
+      theme: {
+        default: { isDark: false },
+      },
+    },
+  }
+</script>
